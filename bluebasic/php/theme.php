0e23161db47c1270d588db544ab5e65a40bec525
BOETZILLA <44696148+BOETZILLA@users.noreply.github.com>
2018-11-04 12:42:32 +0100

    Extended theme description.

--- bluebasic/php/theme.php
+++ bluebasic/php/theme.php
@@ -2,8 +2,8 @@
 
 /**
  *   * Name: Bluebasic
- *   * Description: A new flavour for the Hubzilla standard theme Redbasic
- *   * Version: 1.0
+ *   * Description: You just got yourself some new flavours for your Hubzilla! This theme comes with three different schemes. You can as well use the Hubzilla default scheme "Focus" and fully customize your experience with the custom design settings. Please subscribe to the forum at: <a href="https://nexxt.social/channel/hubzlla_themes" target="blank">hubzlla_themes@nexxt.social</a> to get the latest information on Hubzilla Theme Development. You always find the latest version of this theme at <a href="https://github.com/BOETZILLA/BLUEBASIC_Theme" target="blank">https://github.com/BOETZILLA/BLUEBASIC_Theme</a>.<br/><br/>
+ *   * Version: Theme Version 20181104<br/><br/>
  *   * MinVersion: 2.3.1
  *   * MaxVersion: 6.0
  *   * Author: boet@nexxt.social
