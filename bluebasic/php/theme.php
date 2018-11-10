c62257e81e2dbcff3964cc584f1387dfa4aa7bbe
BOETZILLA <44696148+BOETZILLA@users.noreply.github.com>
2018-11-10 11:06:37 +0100

    Updated version id

--- bluebasic/php/theme.php
+++ bluebasic/php/theme.php
@@ -2,8 +2,8 @@
 
 /**
  *   * Name: Bluebasic
- *   * Description: You just got yourself some new flavours for your Hubzilla! This theme comes with three different schemes. You can as well use the Hubzilla default scheme "Focus" and fully customize your experience with the custom design settings. Please subscribe to the forum at: <a href="https://nexxt.social/channel/hubzlla_themes" target="blank">hubzlla_themes@nexxt.social</a> to get the latest information on Hubzilla Theme Development. You always find the latest version of this theme at <a href="https://github.com/BOETZILLA/BLUEBASIC_Theme" target="blank">https://github.com/BOETZILLA/BLUEBASIC_Theme</a>.<br/><br/>
- *   * Version: Theme Version 20181104<br/><br/>
+ *   * Description: This is a variation of the Hubzilla standard theme "Redbasic". It is heavily inspired by Sean Tilley's work and his "Suckerberg" theme. 
+ *   * Version: Theme Version 20181110<br/><br/>
  *   * MinVersion: 2.3.1
  *   * MaxVersion: 6.0
  *   * Author: boet@nexxt.social
