027ad276eab5b03a07c0b5f563ceb751b3a6487a
BOETZILLA <44696148+BOETZILLA@users.noreply.github.com>
2018-12-02 09:08:32 +0100

    20181201

--- bluebasic/php/theme.php
+++ bluebasic/php/theme.php
@@ -3,7 +3,7 @@
 /**
  *   * Name: Bluebasic
  *   * Description: This is a variation of the Hubzilla standard theme "Redbasic". It is heavily inspired by Sean Tilley's work and his "Suckerberg" theme. 
- *   * Version: Theme Version 20181118<br/><br/>
+ *   * Version: Theme Version 20181201<br/><br/>
  *   * MinVersion: 2.3.1
  *   * MaxVersion: 6.0
  *   * Author: boet@nexxt.social
